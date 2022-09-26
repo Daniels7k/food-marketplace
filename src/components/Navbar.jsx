12c79de1e86c91d45a5e7bc349f0376a8c2ebcc0
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-26 10:47:00 -0300

    Navbar Ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
+import foodYummy from "../assets/FoodYummy.png";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Nav>
+      <div className="brand">
+        <img src={foodYummy} alt="foodYummy" />
+      </div>
+      <ul className="links">
+        <li>
+          <a href="#home" className="active">
+            Home
+          </a>
+        </li>
 
-export default Navbar
+        <li>
+          <a href="#services">
+            Nossos Serviços
+          </a>
+        </li>
+
+        <li>
+          <a href="#portfolio">
+            Portfolio
+          </a>
+        </li>
+
+        <li>
+          <a href="#testimonials">
+            Testemunhas
+          </a>
+        </li>
+
+        <li>
+          <a href="#products">
+            Produtos
+          </a>
+        </li>
+
+        <li>
+          <a href="#newsletter">
+            NewsLetter
+          </a>
+        </li>
+
+      </ul>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 4vw;
+  
+  .brand {
+    img{
+      margin-top: 1rem;
+      cursor: pointer;
+    }
+
+    .toggle {
+      display: none;
+    }
+  }
+
+  .links{
+    display: flex;
+    list-style-type: none;
+    gap: 2rem
+  }
+
+  li{
+    a{
+      color: #fc4958;
+      text-decoration: none;
+      text-transform: uppercase;
+      letter-spacing: 0.2rem;
+      transition: 0.3s ease-in-out;
+      &:hover{
+        color: #f9c74f;
+      }
+    }
+    .active{
+      color: #f9c74f; 
+    }
+  }
+`;
+
+export default Navbar;
