aff62d25326c103193e3c33ea067ecca8b35965f
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-26 16:40:23 -0300

    Formating ReusableStyles component

--- src/components/ReusableStyles.js
+++ src/components/ReusableStyles.js
@@ -1,54 +1,59 @@
 import { css } from "styled-components";
 
 export const TitleStyles = css`
+  .title {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    h1 {
+      font-size: 3rem;
+      text-transform: uppercase;
+
+      span {
+        color: #fc4958;
+      }
+    }
+
+    h2 {
+      span {
+        color: #fc4958 !important;
+      }
+    }
+
+    .yellow {
+      color: #f9c74f;
+    }
+
+    p {
+      padding: 0 10vw;
+      font-size: 1.1rem;
+      line-height: 2rem;
+      letter-spacing: 0.1rem;
+    }
+  }
+
+  @media screen and (min-width: 260px) and (max-width: 1080px) {
     .title {
-        text-align: center;
-        display: flex;
-        flex-direction: column;
-        gap: 2rem,
-        
-        h1{
-            font-size: 3rem;
-            text-transform: uppercase;
-            
-            span {
-                color: #fc4958;
-
-            }
-        }
-
-        .yellow{
-            color: #f9c74f;
-        }
-
-        p{
-            padding: 0 10vw;
-            font-size: 1.1rem;
-            line-height: 2rem;
-            letter-spacing: 0.1rem;
-        }
+      gap: 1rem;
     }
 
-    @media screen and (min-width: 260px) and (max-width: 1080px){
-        .title {
-            gap: 1rem
-        }
-
-        h2{
-            font-size: 2rem;
-        }
-
-        p{
-            padding: 0 1vw;
-        }
+    h2 {
+      font-size: 2rem;
     }
+
+    p {
+      padding: 0 1vw;
+    }
+  }
 `;
 
 export const imageZoomEffect = css`
-    img{
-        transition: 0.8s ease-in-out;
-        &:hover:{
-            padding: 0.1vw
-        }
+  img {
+    transition: 0.8s ease-in-out;
+    &:hover {
+      padding: 0.1vw;
     }
+  }
 `;
