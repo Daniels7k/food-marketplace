c69bdb127c18d38d48d9c12981cbffc974cc22a8
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-27 07:57:26 -0300

    Scroll to top functionality

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 const ScrollToTop = () => {
-  return (
-    <div>ScrollToTop</div>
-  )
-}
+  const [scrollState, setScrollState] = useState(false);
 
-export default ScrollToTop
+  const toTop = () => {
+    window.scrollTo({ top: 0 });
+  };
+
+  window.addEventListener("scroll", () => {
+    window.pageYOffset > 200 ? setScrollState(true) : setScrollState(false);
+  });
+
+  return (
+    <ToTop onClick={toTop} scrollState={scrollState}>
+      <AiOutlineArrowUp />
+    </ToTop>
+  );
+};
+
+const ToTop = styled.div`
+  display: ${({ scrollState }) => (scrollState ? "block" : "none")};
+  position: fixed;
+  cursor: pointer;
+  z-index: 10;
+  bottom: 1rem;
+  right: 2rem;
+  background: linear-gradient(to right, #fc4958, #e85d05);
+  border-radius: 2rem;
+  padding: 1rem 1.15rem;
+
+  svg {
+    font-size: 1.5rem;
+    color: white;
+    transition: 0.6s ease-in-out;
+  }
+
+  &:hover{
+    svg{
+        transform: scale(1.8)
+    }  
+  }
+`;
+export default ScrollToTop;
